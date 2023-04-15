b1ce6e0408a4de5eedad9581dcb5cef717c14281
Yaqub Nasibov <yaqubnasibov00@gmail.com>
2023-04-15 04:13:28 +0400

    Add project files.

---
+++ Consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        ConnectionFactory connectionFactory = new()
+        {
+            Uri = new("amqps://xagvccqi:nlyN6nssoYYaAESqUZuu1G-2GsReIjEe@fly.rmq.cloudamqp.com/xagvccqi")
+        };
+
+        using IConnection connection = connectionFactory.CreateConnection();
+        using IModel channel = connection.CreateModel();
+
+        channel.BasicQos(0, 20, false);
+
+        string queueName = "hello_rabbit_mq";
+        channel.QueueDeclare(queueName, true, false, false);
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (sender, deliveryEventsarg) =>
+        {
+            var messageBody = deliveryEventsarg.Body.ToArray();
+            Thread.Sleep(2500);
+
+            Console.WriteLine($"Message: {Encoding.UTF8.GetString(messageBody)}");
+            channel.BasicAck(deliveryEventsarg.DeliveryTag, false);
+        };
+
+        channel.BasicConsume(queueName, false, consumer);
+        Console.ReadLine();
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        ConnectionFactory connectionFactory = new()
+        {
+            Uri = new Uri("amqps://xagvccqi:nlyN6nssoYYaAESqUZuu1G-2GsReIjEe@fly.rmq.cloudamqp.com/xagvccqi")
+        };
+
+        using IConnection connection = connectionFactory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        string queueName = "hello_rabbit_mq";
+        channel.QueueDeclare(queueName, true, false, false);
+
+        var message = Console.ReadLine();
+
+        Enumerable.Range(1, 60).ToList().ForEach(x =>
+        {
+            var messageBytes = Encoding.UTF8.GetBytes(message + $"{x}");
+            channel.BasicPublish(string.Empty, queueName, null, messageBytes);
+        });
+    }
+}
